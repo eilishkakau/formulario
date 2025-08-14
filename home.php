03b18e73d162af965e384700d3849a60f2416cd9
kaua <carmo.kaua@escola.pr.gov.br>
2025-08-14 15:34:31 -0300

    Update home.php

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SITE | GN</title>
+    <title>SITE</title>
     <style>
         body{
             font-family: Arial, Helvetica, sans-serif;
@@ -34,8 +34,8 @@
     </style>
 </head>
 <body>
-    <h1>Se inscreva no canal ;)</h1>
-    <h2>Prof. Gustavo Neitzke</h2>
+    <h1>Sej bem vindo</h1>
+    <h2>Kauã</h2>
     <div class="box">
         <a href="login.php">Login</a>
         <a href="formulario.php">Cadastre-se</a>
